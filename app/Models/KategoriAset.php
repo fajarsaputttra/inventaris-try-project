b211f854b5120628c4a6a5378cf10de06312eef1
fajarsaputttra <fajar_saputra@teknokrat.ac.id>
2024-04-24 23:46:11 +0700

    make models

---
+++ app/Models/KategoriAset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class KategoriAset extends Model
+{
+    use HasFactory;
+    protected $table = 'kategoriaset';
+}
---
+++ app/Models/LokasiAset.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LokasiAset extends Model
+{
+    use HasFactory;
+    protected $table = 'lokasiaset';
+}
