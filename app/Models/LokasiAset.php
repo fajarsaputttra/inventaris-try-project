e7af358807d565a51fad779d2b79a49817388ecd
fajarsaputttra <fajar_saputra@teknokrat.ac.id>
2024-04-29 13:32:07 +0700

    create style crud

--- app/Models/LokasiAset.php
+++ app/Models/LokasiAset.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class LokasiAset extends Model
 {
     use HasFactory;
-    protected $primaryKey = 'id';
     protected $table = 'lokasiaset';
     protected $guarded = [];
 }
